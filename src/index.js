506cc0d5216be923e1e6f62835fb545fd71e7126
HodoS1209 <bartek.chodowski@gmail.com>
2021-10-05 11:59:36 +0000

    message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = (
-    <h1>Hello World!</h1>
-)
+const element = <h1>Hello World!</h1> 
 
-ReactDOM.render(element, document.getElementById("root"));
+
+class Hello extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "World"
+        }
+    }
+
+    handleChange(event) {
+        const newName = event.target.value;
+        this.setState({ name: newName });
+    }
+
+    render() {
+        return <>
+         <h1>Hello {this.state.name}</h1>
+         <input type="text" onChange={(event) => this.handleChange(event)} />
+        </> 
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById("root"));
